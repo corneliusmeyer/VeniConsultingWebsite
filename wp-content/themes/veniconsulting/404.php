c9c53033008e0811be10e61eeaab372864431f74
corneliusmeyer <cornelius.meyer333@gmail.com>
2022-03-21 15:18:32 +0100

    404 page created + index fix

---
+++ wp-content/themes/veniconsulting/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<h2>Die angeforderte Seite wurde leider nicht gefunden!</h2>
+<p class="py-4"></p>
+<p>Bei Fragen Kontaktieren Sie uns gerne über das <a href="/kontakt">Kontaktformular</a>.</p>
+<p><a href="/">Hier</a> gehts zurück zur Startseite!</p>
+<p>Außerdem finden Sie <a href="/blog">hier</a> unseren aktuellen Blog.</p>
+<?php get_footer(); ?>
